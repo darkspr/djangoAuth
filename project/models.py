0ff4fb8915362e28be173d6997c1e0c014ce8d41
dark <darkspr@126.com>
2023-05-15 17:25:32 +0800

    定义project模型

--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ProjectModel(models.Model):
+    name = models.CharField('项目名', max_length=500)
+    note = models.CharField('备注', max_length=500, null=True, blank=True)
+    add_time = models.DateTimeField('上传时间', auto_now_add=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name = '项目'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f"{self.name}"
